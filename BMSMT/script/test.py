faf037990e757bb4392496795dd3a64a1d1f5563
Your Name <you@example.com>
2021-04-10 18:03:58 +0800

    update

--- BMSMT/script/test.py
+++ BMSMT/script/test.py
@@ -8,14 +8,14 @@ test['table']   = data.tabulation.skip(table=data.tabulation.read("SOURCE/CSV/AN
 test['dataset'] = data.dataset(test['table'], image=data.process.image.review, target=data.process.target.review)
 
 ##
-loader = data.loader(test=test['dataset'], batch=8)
+loader = data.loader(test=test['dataset'], batch=32)
 
 ##
 model  = network.model()
 model.load(path='SOURCE/LOG/3.weight')
 
 ##
-machine = network.machine(model=model, device='cuda', folder="SOURCE/PICKLE", checkpoint='test')
+machine = network.machine(model=model, device='cuda', folder="SOURCE/LOG", checkpoint='test')
 machine.measure(test=loader.test)
 machine.save()
 
