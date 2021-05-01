fbf8b44a6f40a6bda86eecca8dc3bc3e2ed3cacf
Your Name <you@example.com>
2021-05-01 16:29:08 +0800

    V2
    修改修改

--- BMSMT/script/holdout.py
+++ BMSMT/script/holdout.py
@@ -7,7 +7,7 @@ table = data.tabulation.read("SOURCE/CSV/ANNOTATION.csv")
 table = data.tabulation.filter(table=table, column='mode', value='train')
 
 ##  Debug or not.
-debug = True
+debug = False
 if(debug):
 
     number = round(len(table)/1000)
@@ -26,7 +26,7 @@ train['dataset'] = data.dataset(train['table'], image=data.process.image.learn ,
 check['dataset'] = data.dataset(check['table'], image=data.process.image.review, target=data.process.target.review)
 
 ##
-loader = data.loader(train=train['dataset'], check=check['dataset'], batch=4)
+loader = data.loader(train=train['dataset'], check=check['dataset'], batch=108)
 if(loader.available("train") and loader.available("check")):
 
     print("Loader work successfully.")
@@ -53,25 +53,28 @@ for epoch in range(iteration):
 
     ##  Learning process.
     machine.learn(loader.train)
-    machine.measure(train=loader.train, check=loader.check)
-    machine.save("checkpoint")
-    machine.save("measurement")
-    machine.update('checkpoint')
 
-    ##  Measurement.
-    measurement = machine.measurement
-    
-    ##  History of epoch.
-    item = [network.metric.cel(measurement['train']['target'][i,:], measurement['train']['likelihood'][i,:,:], label=range(94)) for i in range(train['size'])]
-    history['train']['cel'] += [sum(item)/len(item)]
-    item = [network.metric.cel(measurement['check']['target'][i,:], measurement['check']['likelihood'][i,:,:], label=range(94)) for i in range(check['size'])]
-    history['check']['cel'] += [sum(item)/len(item)]
-    
-    ##  Save the report.
-    report = network.report(train=history['train'], check=history['check'])
-    report.summarize()
-    report.save(folder=folder)
-    pass
+    if(epoch%5==0):
+
+        machine.measure(train=loader.train, check=loader.check)
+        machine.save("checkpoint")
+        machine.save("measurement")
+        machine.update('checkpoint')
+
+        ##  Measurement.
+        measurement = machine.measurement
+        
+        ##  History of epoch.
+        item = [network.metric.cel(measurement['train']['target'][i,:], measurement['train']['likelihood'][i,:,:], label=range(94)) for i in range(train['size'])]
+        history['train']['cel'] += [sum(item)/len(item)]
+        item = [network.metric.cel(measurement['check']['target'][i,:], measurement['check']['likelihood'][i,:,:], label=range(94)) for i in range(check['size'])]
+        history['check']['cel'] += [sum(item)/len(item)]
+        
+        ##  Save the report.
+        report = network.report(train=history['train'], check=history['check'])
+        report.summarize()
+        report.save(folder=folder)
+        pass
 
 
 
