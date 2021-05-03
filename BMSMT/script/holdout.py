5ebddbec000c15df6d7c4336388f0bbcfce168f2
Your Name <you@example.com>
2021-05-03 18:16:26 +0800

    update

--- BMSMT/script/holdout.py
+++ BMSMT/script/holdout.py
@@ -54,29 +54,51 @@ for epoch in range(iteration):
     ##  Learning process.
     machine.learn(loader.train)
 
-    if(epoch%5==0):
-
-        machine.measure(train=loader.train, check=loader.check)
-        machine.save("checkpoint")
-        machine.save("measurement")
-        machine.update('checkpoint')
-
-        ##  Measurement.
-        measurement = machine.measurement
+    machine.measure(train=loader.train, check=loader.check)
+    machine.save("checkpoint")
+    machine.save("measurement")
+    machine.update('checkpoint')
+    
+    ##  Measurement.
+    measurement = machine.measurement
         
-        ##  History of epoch.
-        history['train']['cost'] += [measurement['train']['cost']]
-        history['check']['cost'] += [measurement['check']['cost']]
-        # item = [network.metric.cel(measurement['train']['target'][i,:], measurement['train']['likelihood'][i,:,:], label=range(94)) for i in range(train['size'])]
-        # history['train']['cost'] += [sum(item)/len(item)]
-        # item = [network.metric.cel(measurement['check']['target'][i,:], measurement['check']['likelihood'][i,:,:], label=range(94)) for i in range(check['size'])]
-        # history['check']['cost'] += [sum(item)/len(item)]
+    ##  History of epoch.
+    history['train']['cost'] += [measurement['train']['cost']]
+    history['check']['cost'] += [measurement['check']['cost']]
+    # item = [network.metric.cel(measurement['train']['target'][i,:], measurement['train']['likelihood'][i,:,:], label=range(94)) for i in range(train['size'])]
+    # history['train']['cost'] += [sum(item)/len(item)]
+    # item = [network.metric.cel(measurement['check']['target'][i,:], measurement['check']['likelihood'][i,:,:], label=range(94)) for i in range(check['size'])]
+    # history['check']['cost'] += [sum(item)/len(item)]
+    
+    ##  Save the report.
+    report = network.report(train=history['train'], check=history['check'])
+    report.summarize()
+    report.save(folder=folder)
+    pass
+
+    # if(epoch%5==0):
+
+    #     machine.measure(train=loader.train, check=loader.check)
+    #     machine.save("checkpoint")
+    #     machine.save("measurement")
+    #     machine.update('checkpoint')
+
+    #     ##  Measurement.
+    #     measurement = machine.measurement
         
-        ##  Save the report.
-        report = network.report(train=history['train'], check=history['check'])
-        report.summarize()
-        report.save(folder=folder)
-        pass
+    #     ##  History of epoch.
+    #     history['train']['cost'] += [measurement['train']['cost']]
+    #     history['check']['cost'] += [measurement['check']['cost']]
+    #     # item = [network.metric.cel(measurement['train']['target'][i,:], measurement['train']['likelihood'][i,:,:], label=range(94)) for i in range(train['size'])]
+    #     # history['train']['cost'] += [sum(item)/len(item)]
+    #     # item = [network.metric.cel(measurement['check']['target'][i,:], measurement['check']['likelihood'][i,:,:], label=range(94)) for i in range(check['size'])]
+    #     # history['check']['cost'] += [sum(item)/len(item)]
+        
+    #     ##  Save the report.
+    #     report = network.report(train=history['train'], check=history['check'])
+    #     report.summarize()
+    #     report.save(folder=folder)
+    #     pass
 
 
 
