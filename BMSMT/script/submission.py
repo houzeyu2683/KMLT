69c308ea9975b415e939374bfc3b9c939447a752
Your Name <you@example.com>
2021-05-19 19:23:23 +0800

    a

--- BMSMT/script/submission.py
+++ BMSMT/script/submission.py
@@ -7,7 +7,7 @@ table = data.tabulation.read("SOURCE/CSV/ANNOTATION.csv")
 table = data.tabulation.filter(table=table, column='mode', value='test')
 
 ##  Debug or not.
-debug = True
+debug = False
 if(debug):
 
     number = round(len(table)/4000)
@@ -19,14 +19,14 @@ group = {}
 group['size'] = 4
 group['table']   = [value['check'] for _, value in data.validation.fold(table=table, size=group['size']).items()]
 group['dataset'] = [data.dataset(i, image=data.process.image.review , text=data.process.text.tokenize) for i in group['table']] 
-group['loader']  = [data.loader(test=i, batch=8).test for i in group['dataset']]
+group['loader']  = [data.loader(test=i, batch=16).test for i in group['dataset']]
 
 ##
 model = network.model()
 
 ##
 machine = network.machine(model=model, device='cuda')
-machine.load(what='weight', path="SOURCE/LOG/2.checkpoint")
+machine.load(what='weight', path="SOURCE/LOG/9.checkpoint")
 
 ##
 def submit(index, machine, group):
